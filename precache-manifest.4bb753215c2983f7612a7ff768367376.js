248422b2fae5375e4b884ecfac825ab068154aa3
unknown <Leo hengebaert>
2021-11-27 13:44:08 +0100

    Updates

---
+++ precache-manifest.4bb753215c2983f7612a7ff768367376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7acbeadb69f4bc7a9f64f600f6ec3d6",
+    "url": "/sidebar-react/index.html"
+  },
+  {
+    "revision": "85ad3abd1809ef4b6799",
+    "url": "/sidebar-react/static/css/main.1109858f.chunk.css"
+  },
+  {
+    "revision": "43dfabc4b429df1fba1a",
+    "url": "/sidebar-react/static/js/2.6909725c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sidebar-react/static/js/2.6909725c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ad3abd1809ef4b6799",
+    "url": "/sidebar-react/static/js/main.319072d1.chunk.js"
+  },
+  {
+    "revision": "f84bf6c77999775c55b4",
+    "url": "/sidebar-react/static/js/runtime-main.423fb0bf.js"
+  }
+]);
